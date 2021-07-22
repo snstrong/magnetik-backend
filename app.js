c5c0eab94170d9d828cbd8405dcac636f137597d
Sara Strong <sara.n.strong@gmail.com>
2021-07-22 15:30:10 -0400

    Added db.js, expressError.js

--- app.js
+++ app.js
@@ -2,20 +2,26 @@
 
 /** Express app for Magnetik */
 
-// Required packages
+// Packages
 //
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-// Required files
+// Files
 //
+const { NotFoundError } = require("./expressError");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
 
 /** Generic error handler; anything unhandled ends up here. */
 app.use(function (err, req, res, next) {
