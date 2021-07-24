f14374dfdc27558872d659cb9163fc61eff7ddaf
Sara Strong <sara.n.strong@gmail.com>
2021-07-24 17:35:45 -0400

    JWTs and auth middleware

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-/** Handle 404 errors -- this matches everything */
+/** Handle 404 errors */
 app.use(function (req, res, next) {
   return next(new NotFoundError());
 });
