7e284679e4e781f90d0144c8603994e35d417637
Sara Strong <sara.n.strong@gmail.com>
2021-10-24 17:18:41 -0400

    boilerplate for post, patch writespace routes

--- routes/writespaces.js
+++ routes/writespaces.js
@@ -3,18 +3,21 @@
 const express = require("express");
 
 const Writespace = require("../models/writespace");
-
+const {
+  authenticateJWT,
+  ensureCorrectUserOrAdmin,
+} = require("../middleware/auth");
 const router = new express.Router();
 
-/**
- *
+/** GET / => { wordList }
  * Gets a new word list. Uses default constraints defined in Writespace model.
  *
  * Returns word list.
  *
+ * Authorization required: none.
  */
 
-router.get("/", async function (req, res, next) {
+router.get("/", authenticateJWT, async function (req, res, next) {
   try {
     const wordList = await Writespace.getWordList();
     return res.status(200).json({ wordList });
@@ -22,5 +25,54 @@ router.get("/", async function (req, res, next) {
     return next(err);
   }
 });
+
+/** POST /[username] {writespaceData} / => {"created": {writespaceId, username}}
+ *
+ * Creates a new writespace.
+ *
+ * Returns {writespaceId, username}
+ *
+ * Authorization required: admin or same user.
+ * */
+
+router.post(
+  "/:username",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      // validate w/ JSON schema
+      // post to db
+      // if success, return
+      // let WritespaceId = res.id
+      // return res.status(201).json({writespaceId, username});
+      return;
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** PATCH /[username]/[writespaceId] => {"updated": {writespaceId, username}}
+ *
+ * Updates a writespace.
+ *
+ * Returns {"updated": {writespaceId, username}}
+ *
+ * Authorization required: admin or same user.
+ */
+
+router.patch(
+  "/:username/:writespaceId",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      // validate w/ JSON schema
+      // send update to db
+      // if success, return {"updated": {writespaceId, username}}
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
 
 module.exports = router;
