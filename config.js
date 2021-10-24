e0e9e17ad41bb8398a24ac47e155b8e439888300
Sara Strong <sara.n.strong@gmail.com>
2021-10-24 15:04:21 -0400

    Add pg-format for batch db inserts

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Shared config for application; can be required many places. */
+/** Shared config for application */
 
 require("dotenv").config();
 require("colors");
@@ -19,7 +19,7 @@ function getDatabaseUri() {
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+// Should this be increased to 13 for non-test use?
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("Magnetik Config:".green);
--- csvImport.js
+++ csvImport.js
@@ -1,14 +1,8 @@
-/** Based on tutorial at https://www.bezkoder.com/node-js-csv-postgresql/ */
-
-/** see ISSUE */
+/** Import word data from csv file to postgresql database */
 
 const fs = require("fs");
-// const Pool = require("pg").Pool;
-// const { Client } = require("pg");
-// const { getDatabaseUri } = require("./config");
 const fastcsv = require("fast-csv");
-// fast-csv docs: https://c2fo.github.io/fast-csv/
-
+const format = require("pg-format");
 const db = require("./db");
 
 let stream = fs.createReadStream("magnetik_word_data.csv");
@@ -19,66 +13,16 @@ let csvStream = fastcsv
     csvData.push(data);
   })
   .on("end", function () {
-    // remove the first line: header
+    // remove the first line header
     csvData.shift();
-
-    const query = "INSERT INTO words (word, pos_tag) VALUES ($1, $2)";
-
-    /** Initially tried using a Pool rather than a Client, but it errored out halfway through the call to forEach in ln 50. Also tried instantiating a new Client instance instead of importing db from db.js, but did not solve issue. */
-
-    // // create a new connection to the database
-    // const pool = new Pool({
-    //   host: "localhost",
-    //   user: "postgres",
-    //   database: getDatabaseUri(),
-    //   port: 5432,
-    // });
-
-    // pool.connect((err, client, done) => {
-    //   if (err) throw err;
-
-    //   try {
-    //     csvData.forEach((row) => {
-    //       client.query(query, row, (err, res) => {
-    //         if (err) {
-    //           console.log(err.stack);
-    //         } else {
-    //           console.log("inserted " + res.rowCount + " row:", row);
-    //         }
-    //       });
-    //     });
-    //   } finally {
-    //     done();
-    //   }
-    // });
-
+    const query = "INSERT INTO words (word, pos_tag) VALUES %L";
     try {
-      csvData.forEach((row) => {
-        db.query(query, row).then((res) => {
-          console.log("Inserted " + res.rowCount + " row:", row);
-        });
-      });
+      db.query(format(query, csvData)).then(() =>
+        console.log("Insert successful")
+      );
     } catch (err) {
-      console.error("Error in try/catch", err);
+      console.error("Error inserting into db", err);
     }
-
-    /** Calling process.exit does not work after the try/catch whether it is in a finally block or not */
-    // process.exit();
-
-    /**
-    ISSUE: calling db.end, even when using try/catch or .catch(), throws the following error:
-    
-        (node:32851) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode).
-
-    As things stand, the code runs and populates the database successfully, but the process must be terminated in the console once it reaches its conclusion.
-    
-    This error occurs if db.end is placed at the end of the try block or after the catch block.
-    
-    Placing it after the call to stream.pipe at the end of the file causes the connection to terminate prematurely and still throws an error. So does calling process.exit, or chaining on an .on('end') to stream.pipe
-    */
   });
 
 stream.pipe(csvStream);
-
-/** This didn't work, either: */
-// csvStream.end();
